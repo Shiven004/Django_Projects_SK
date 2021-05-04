25faabff1b0abeb8ec5fb0e107f2430bd43d062a
Shivender Khajuria <shivenkhajuria@yahoo.com>
2021-05-04 14:58:13 +0530

    Update settings.py
    Second commit!!!

--- django_project_01/django_project_01/settings.py
+++ django_project_01/django_project_01/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '^6f_cr@#^#p=%ao9c5$2b#vv&rbbp4j06nu0#zed-kya1w_()1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://shivenblogapp.herokuapp.com/']
 
 
 # Application definition
